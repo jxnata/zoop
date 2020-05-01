e289e30eb2a7dcd1ffeff56240fe8cded4bf9ecb
John <jxnata@iMac-de-John.local>
2020-05-01 15:57:03 -0300

    Export from index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+export * from './lib/boleto';
+export * from './lib/buyer';
+export * from './lib/endpoint-error';
+export * from './lib/endpoint';
+export * from './lib/marketplace';
+export * from './lib/resource';
+export * from './lib/seller';
+export * from './lib/token';
+export * from './lib/transaction';
+export * from './lib/verification-status';
+export * from './lib/zoop';
+
+import { Zoop } from './lib/zoop';
+
+export default Zoop;
